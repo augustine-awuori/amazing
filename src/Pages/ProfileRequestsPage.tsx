83016a3babc6238ffcb73a90e87555cf9ee08ced
augustine-awuori <codewithaugustine@gmail.com>
2023-08-24 23:25:15 +0300

    Create profile requests page.

--- src/Pages/ProfileRequestsPage.tsx
+++ src/Pages/ProfileRequestsPage.tsx
@@ -1,7 +1,43 @@
-import React from "react";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+
+import { Category } from "../hooks/useCategories";
+import { endpoint } from "../services/requests";
+import { useProfileRequests } from "../hooks";
+import empty from "../utilities/empty";
+import GridPageContainer from "../components/GridPageContainer";
+import manipulator from "../utilities/stringManipulator";
+import RequestGrid from "../components/RequestGrid";
+import useRequest, { Request } from "../hooks/useRequest";
 
 const ProfileRequestsPage = () => {
-  return <div>ProfileRequestsPage</div>;
+  const navigate = useNavigate();
+  const { setRequest } = useRequest();
+  const params = useParams();
+  const { error, isLoading, requests } = useProfileRequests(
+    params[empty.user.paramsId]
+  );
+  const [selectedCategory, setSelectedCategory] = useState<Category | null>(
+    null
+  );
+
+  const navigateToDetails = (request: Request) => {
+    setRequest(request);
+    navigate(`${endpoint}/${request._id}`);
+  };
+
+  return (
+    <GridPageContainer
+      headingPrefix={manipulator.getFirstWord(requests?.[0]?.author?.name)}
+      selectedCategory={selectedCategory}
+      onSelectCategory={setSelectedCategory}
+    >
+      <RequestGrid
+        onRequestClick={navigateToDetails}
+        selectedCategory={selectedCategory}
+      />
+    </GridPageContainer>
+  );
 };
 
 export default ProfileRequestsPage;
--- src/hooks/useProfileRequests.ts
+++ src/hooks/useProfileRequests.ts
@@ -1,5 +1,36 @@
+import { useContext, useEffect, useState } from "react";
+
+import ProfileRequestsContext from "../contexts/ProfileRequestsContext";
+import requestsApi from "../services/requests";
+
 const useProfileRequests = (userId: string) => {
-  return { count: 0 };
+  const context = useContext(ProfileRequestsContext);
+  const [isLoading, setLoading] = useState(false);
+  const [error, setError] = useState("");
+
+  const profileRequests = context?.profileRequests || [];
+  const setProfileRequests = context?.setProfileRequests;
+
+  useEffect(() => {
+    getRequests();
+  }, [userId]);
+
+  const getRequests = async () => {
+    if (userId) {
+      setLoading(true);
+      const { data, ok, problem } = await requestsApi.getUserRequests(userId);
+      setLoading(false);
+      if (ok) setProfileRequests(data);
+      else setError(data?.error || problem);
+    }
+  };
+
+  return {
+    count: profileRequests.length,
+    error,
+    isLoading,
+    requests: profileRequests,
+  };
 };
 
 export default useProfileRequests;
