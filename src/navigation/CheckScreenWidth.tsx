017a2f5f84d9a8f525ec15ca7588f8f21abde568
augustine-awuori <codewithaugustine@gmail.com>
2023-12-12 13:34:18 +0300

    Fix the layout issue on small devices

--- src/navigation/CheckScreenWidth.tsx
+++ src/navigation/CheckScreenWidth.tsx
@@ -3,7 +3,7 @@ import { useBreakpointValue } from "@chakra-ui/react";
 import TopBar from "../components/nav/TopBar";
 
 const CheckScreenWidth = ({ Component }: { Component: JSX.Element }) => {
-  const isSmallScreen = useBreakpointValue({ sm: true, md: false });
+  const isSmallScreen = useBreakpointValue({ base: true, sm: true, md: false });
 
   if (isSmallScreen) return <TopBar />;
 
