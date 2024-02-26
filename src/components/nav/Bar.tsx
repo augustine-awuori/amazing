b24c125bc7b5f74af4c20b87b056859bee38605a
augustine-awuori <codewithaugustine@gmail.com>
2024-02-26 16:11:57 +0300

    Change logo

--- src/components/nav/Bar.tsx
+++ src/components/nav/Bar.tsx
@@ -64,7 +64,7 @@ export default function WithSubNav({ cartCount, user }: Props) {
             fontWeight="bold"
             fontSize={18}
           >
-            Amazing
+            amazing
           </Text>
         </Flex>
 
