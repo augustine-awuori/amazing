deff37bc951b91602ecd94ee38b78e61df2313e0
augustine-awuori <codewithaugustine@gmail.com>
2024-05-18 13:25:39 +0300

    Code refactor

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ import { generateMessagingToken } from "./db/notification";
 import { NavBar, Routes } from "./components";
 import { User } from "./hooks/useUser";
 import auth from "./services/auth";
-import ColumnContext from "./contexts/ColumnContext";
 
 function App() {
   const [user, setUser] = useState<User | null>();
-  const [column, setColumn] = useState("220px");
   const [cartProducts, setCartProducts] = useState<CartProducts>({
     count: 0,
     ids: {},
@@ -26,25 +24,20 @@ function App() {
   };
 
   return (
-    <ColumnContext.Provider value={{ column, setColumn }}>
-      <Grid
-        templateAreas={{
-          base: `"nav""main"`,
-          lg: `"nav nav" "aside main"`,
-        }}
-        templateColumns="100%"
-      >
-        <GridItem area="nav">
-          <NavBar user={user} cartCount={cartProducts.count} />
-        </GridItem>
-        <Box pt={{ base: "3.5rem", md: "2.1rem" }}>
-          <Routes
-            cartProducts={cartProducts}
-            setCartProducts={setCartProducts}
-          />
-        </Box>
-      </Grid>
-    </ColumnContext.Provider>
+    <Grid
+      templateAreas={{
+        base: `"nav""main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+      templateColumns="100%"
+    >
+      <GridItem area="nav">
+        <NavBar user={user} cartCount={cartProducts.count} />
+      </GridItem>
+      <Box pt={{ base: "3.5rem", md: "2.1rem" }}>
+        <Routes cartProducts={cartProducts} setCartProducts={setCartProducts} />
+      </Box>
+    </Grid>
   );
 }
 
