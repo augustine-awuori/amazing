4652d2169a195450f148f16135b7f0054e7621ac
augustine-awuori <codewithaugustine@gmail.com>
2024-05-20 07:54:00 +0300

    Restore service worker file

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,29 @@
+// eslint-disable-next-line no-undef
+importScripts("https://www.gstatic.com/firebasejs/8.10.1/firebase-app.js");
+// eslint-disable-next-line no-undef
+importScripts(
+  "https://www.gstatic.com/firebasejs/8.10.1/firebase-messaging.js"
+);
+
+// eslint-disable-next-line no-undef
+firebase.initializeApp({
+  apiKey: "AIzaSyCAtitgurCoK8LIYRWfo2i95Q6otoTmXSA",
+  authDomain: "kisii-campus-mart-site.firebaseapp.com",
+  projectId: "kisii-campus-mart-site",
+  storageBucket: "kisii-campus-mart-site.appspot.com",
+  messagingSenderId: "66759292374",
+  appId: "1:66759292374:web:2a09e7ad0919c6a056e077",
+  measurementId: "G-C2MJ2XQDCQ",
+});
+
+// eslint-disable-next-line no-undef
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(({ notification }) => {
+  const notificationOptions = {
+    body: notification.body,
+    icon: notification.icon,
+  };
+
+  self.registration.showNotification(notification?.title, notificationOptions);
+});
